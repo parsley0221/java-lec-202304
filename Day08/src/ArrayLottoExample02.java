632ec8369a2c263d3d73a70b74827ed05de8fc9c
parsley0221 <02_15@naver.com>
2023-05-17 09:20:11 +0900

    숙제 업로드

--- Day08/src/ArrayLottoExample02.java
+++ Day08/src/ArrayLottoExample02.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // lotto 6/45
 // lotto 6/x
 // java ArrayLottoExample 1000
@@ -5,25 +7,29 @@
 // 2번 x/y 예를 들면 java ArrayLottoExample 10 100
 // 1-100까지의 수에서 10개를 뽑아서 출력하기
 
-public class ArrayLottoExample01 {
+public class ArrayLottoExample02 {
 	
 	public static void main(String[] args) {
 		//System.out.println(args[0]); // Run 하면 실제값이 없는데 0을 넣어 에러가 발생한다. run > config ... 해서 값을 넣어주면 출력된다.
 		
+		System.out.println
+		("Lotto <뽑을개수> 입력해주세요.");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int pickCount = scan.nextInt();
 				
-		if(args.length == 0 || args.length > 2) {
-			System.out.println
-			("사용법 : java ArrayLottoExample <뽑을개수> <최대숫자>");
-			return;			
-		}
+		System.out.println
+		("Lotto <최대숫자> 입력해주세요.");
 		
-		// 뽑을 수의 개수
-		int pickCount = Integer.parseInt(args[0]);
+		Scanner scan0 = new Scanner(System.in);
 		
+		int maxNum = scan0.nextInt();
+		
+		scan0.close();
+
 		//-- 사용법 안내 통과 후 진행하는 곳
-		int maxNum = Integer.parseInt(args[1]); // 문자로 들어온 숫자를 인티저 숫자로 변경해준다. run > config... 에 쓴 값으로 진행.
-						// 최대 숫자 ( ex : lotto 6/100, 6/200 )
-						// 사용자가 입력한 값을 넣는 변수		
+
 		int[] lotto = new int[pickCount]; // 로또는 N개를 추첨하기 때문
 		
 		
@@ -32,7 +38,11 @@ public class ArrayLottoExample01 {
 			// 랜덤한 값을 집어넣습니다.
 			int num = (int)(Math.random() * maxNum) + 1; //ramdom 실수형이기때문에 (int) 변경을 해야 에러가 사라진다.
 			// +1은 정수변환 될 때 0부터 시작되기 때문
+			
 			lotto[y] = num;
+			
+			
+			
 		}
 		
 		//-- 로또번호 출력하기
